18f22b90b7b1744bd6c5ed0338396223ab2bcebf
Viktomm <misterviktor@bk.ru>
2026-01-21 21:50:53 +0300

    2.4.1

--- js/script.js
+++ js/script.js
@@ -9,7 +9,9 @@ const skills = {
         {name: 'css', range: 40, icon: 'css'},
         {name: 'eng', range: 70, icon: 'english'}
     ],
+    sortMode: null,
     generateList(parentElement) {
+        parentElement.innerHTML = '';
         const skillList = parentElement;
         this.data.forEach(element => {
             let dt = document.createElement('dt');
@@ -26,9 +28,58 @@ const skills = {
             skillList.appendChild(dt);
             skillList.appendChild(dd);
         });
+    },
+    sortList(typeSort) {
+        if (this.sortMode !== typeSort) {
+            this.sortMode = typeSort
+            this.data.sort(getComparer(typeSort));
+        } else {
+            this.data.reverse();
+        }
+        this.generateList(skillList);
     }
-
 }
 
 const skillList = document.querySelector('.skill-list');
 skills.generateList(skillList);
+
+const buttonsBlock = document.querySelector('.sort-options');
+buttonsBlock.addEventListener('click', (e) => {
+    let target = e.target
+    if (target.nodeName === "BUTTON") {
+        switch (target.dataset.type) {
+            case 'name':
+            case 'range':
+                skills.sortList(target.dataset.type);
+                break;
+            default:
+                console.log('Нажатие неизвестной кнопки');
+        }
+    }
+});
+
+function compare(a, b) {
+    if (a.name < b.name) {
+        return -1;
+    }
+
+    if (a.name > b.name) {
+        return 1;
+    }
+
+    return 0;
+}
+
+function getComparer(prop) {
+    return function (a, b) {
+        if (a[prop] < b[prop]) {
+            return -1;
+        }
+
+        if (a[prop] > b[prop]) {
+            return 1;
+        }
+
+        return 0;
+    }
+}
