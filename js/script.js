323082f621d7188c14bb04e5f59f2e7d37e011a8
Viktomm <misterviktor@bk.ru>
2026-01-22 03:06:41 +0300

    2.7

--- js/script.js
+++ js/script.js
@@ -1,33 +1,50 @@
 "use strict";
 const skills = {
-    data : [
-        {name: 'java', range: 90, icon: 'java'},
-        {name: 'node', range: 60, icon: 'nodejs'},
-        {name: 'js', range: 50, icon: 'javascript'},
-        {name: 'cpp', range: 60, icon: 'c++'},
-        {name: 'html', range: 40, icon: 'html'},
-        {name: 'css', range: 40, icon: 'css'},
-        {name: 'eng', range: 70, icon: 'english'}
-    ],
+    data: [],
     sortMode: null,
-    generateList(parentElement) {
+    errorMessage: "Ошибка сервера",
+    async generateList(parentElement, path = null) {
         parentElement.innerHTML = '';
-        const skillList = parentElement;
-        this.data.forEach(element => {
-            let dt = document.createElement('dt');
-            let dd = document.createElement('dd');
-            let div = document.createElement('div');
+        if (this.data.length > 0) {
+            this.data.forEach(element => {
+                let dt = document.createElement('dt');
+                let dd = document.createElement('dd');
+                let div = document.createElement('div');
 
-            dt.style = `background-image: url('img/skill=${element.icon}.svg')`;
-            dd.classList.add('skill-level');          
-            div.textContent = `${element.range}%`;
-            div.style.width = `${element.range}%`;   
-            dt.textContent = element.name;
+                dt.style = `background-image: url('img/skill=${element.icon}.svg')`;
+                dd.classList.add('skill-level');          
+                div.textContent = `${element.range}%`;
+                div.style.width = `${element.range}%`;   
+                dt.textContent = element.name;
 
-            dd.appendChild(div);
-            skillList.appendChild(dt);
-            skillList.appendChild(dd);
-        });
+                dd.appendChild(div);
+                skillList.appendChild(dt);
+                skillList.appendChild(dd);
+            });
+        } else {
+            parentElement.textContent = this.errorMessage;
+
+            const buttonReGenerateList = document.createElement('button');
+            buttonReGenerateList.textContent = "Обновить";
+
+            buttonReGenerateList.addEventListener('click', () => {
+                this.setData(path).then(
+                    (path) => this.generateList(parentElement, path)
+                ).then(
+                    () => {
+                        if (parentElement.textContent == (this.errorMessage + 'Обновить')) {
+                            buttonsBlock.style.display = "None";
+                        }
+                        else {
+                            buttonsBlock.style.display = "flex";
+                        }
+                    }
+                );
+                console.log(`Попытка повторного получения '${path}'... `);
+            });
+
+            parentElement.append(buttonReGenerateList);
+        }
     },
     sortList(typeSort) {
         if (this.sortMode !== typeSort) {
@@ -37,11 +54,27 @@ const skills = {
             this.data.reverse();
         }
         this.generateList(skillList);
+    },
+    async setData(path) {
+        await fetch(path)
+            .then(data => data.json())
+            .then(object => this.data = object)
+            .catch((err) => console.error(err));
+        return path;
     }
 }
 
 const skillList = document.querySelector('.skill-list');
-skills.generateList(skillList);
+skills.setData('../db/skills.json')
+    .then(
+        (path) => skills.generateList(skillList, path))
+    .then(
+        () => {
+            if (skillList.textContent == (skills.errorMessage + 'Обновить')) {
+                buttonsBlock.style.display = "None";
+            }
+        }
+    );
 
 const buttonsBlock = document.querySelector('.sort-options');
 buttonsBlock.addEventListener('click', (e) => {
